931f627846e5e50746da83da7a111cb6d8049365
Artavazd Barseghyan <artavazd1@gmail.com>
2025-09-11 11:19:49 +0200

    update transport

--- src/http-transport.adapter.ts
+++ src/http-transport.adapter.ts
@@ -2,24 +2,25 @@ import http, { createServer, IncomingMessage, ServerResponse, Server } from 'htt
 import { v4 as uuidv4 } from 'uuid';
 import { ZodError } from 'zod';
 import { CorrelatedRequestDTO, CorrelatedResponseDTO, TransportAdapter, transportService } from 'transport-pkg';
-import { AppRunPriority } from 'app-life-cycle-pkg';
+import { IAppPkg, AppRunPriority } from 'app-life-cycle-pkg';
 import { BaseError, BadRequestError, InternalServerError } from 'rest-pkg';
 import { httpLogger } from 'common-loggers-pkg';
 
 const HTTP_TRANSPORT_ENDPOINT = '/http-transport';
 
-class HTTPTransportAdapter implements TransportAdapter {
+class HTTPTransportAdapter extends TransportAdapter implements IAppPkg {
   private server: Server | null = null;
-  private pendingResponses: Map<string, (message: CorrelatedResponseDTO) => void> = new Map();
   private port: number;
 
   constructor(port: number = 0) {
+    super();
+
     this.port = port;
   }
 
   async init(): Promise<void> {
-    const actionsToConsume: Record<string, (data: CorrelatedRequestDTO) => Promise<void>> = transportService.getReceivableActions();
-    if (!Object.keys(actionsToConsume).length) {
+    const actionHandlers: Record<string, (data: CorrelatedRequestDTO) => Promise<object>> = transportService.getActionHandlers();
+    if (!Object.keys(actionHandlers).length) {
       return;
     }
 
@@ -67,27 +68,11 @@ class HTTPTransportAdapter implements TransportAdapter {
       data.request_id = uuidv4();
     }
 
-    const actionsToProduce: string[] = transportService.getSendableActions();
-    if (!actionsToProduce.includes(data.action)) {
-      throw new BadRequestError(`Invalid action provided: ${data.action}`);
-    }
-
     if (!options['host'] || !options['port']) {
       throw new BadRequestError('Host and/or port missing in transport options');
     }
 
     return await this.sendHTTPRequest(data, options, timeout);
-  }
-
-  async sendResponse(data: CorrelatedResponseDTO): Promise<void> {
-    if (data.request_id && this.pendingResponses.has(data.request_id)) {
-      const resolve = this.pendingResponses.get(data.request_id);
-      if (resolve) {
-        httpLogger.info(`Response sent: ${JSON.stringify(data)}`);
-        resolve(data);
-        this.pendingResponses.delete(data.request_id);
-      }
-    }
   }
 
   private getRequestUrl(options: Record<string, unknown>) {
@@ -137,15 +122,15 @@ class HTTPTransportAdapter implements TransportAdapter {
     });
   }
 
-  private async handleHTTPRequest(data: CorrelatedRequestDTO | null, res: ServerResponse) {
-    const actionsToConsume: Record<string, (data: CorrelatedRequestDTO) => Promise<void>> = transportService.getReceivableActions();
+  private async handleHTTPRequest(data: CorrelatedRequestDTO | null, res: ServerResponse): Promise<void> {
+    const actionHandlers: Record<string, (data: CorrelatedRequestDTO) => Promise<object>> = transportService.getActionHandlers();
 
     try {
-      if (!data || !(data.action in actionsToConsume)) {
+      if (!data || !actionHandlers[data.action]) {
         throw new BadRequestError(`Invalid action provided: ${data?.action}`);
       }
 
-      const handler = actionsToConsume[data.action];
+      const handler: (data: CorrelatedRequestDTO) => Promise<object> = actionHandlers[data.action];
       if (!handler) {
         throw new InternalServerError(`Handler not defined for action: ${data.action}`);
       }
@@ -165,34 +150,13 @@ class HTTPTransportAdapter implements TransportAdapter {
     }
   }
 
-  private async executeActionHandler(handler: (data: CorrelatedRequestDTO) => Promise<void>, data: CorrelatedRequestDTO, res: ServerResponse) {
+  private async executeActionHandler(handler: (data: CorrelatedRequestDTO) => Promise<object>, data: CorrelatedRequestDTO, res: ServerResponse): Promise<void> {
     let status = 200;
     let errorMessage: string = '';
-    let response: CorrelatedResponseDTO | null = null;
+    let responseData: object | null = null;
 
     try {
-      response = await new Promise<CorrelatedResponseDTO>(async (resolve, reject) => {
-        const timer = setTimeout(() => {
-          reject(new InternalServerError(`Timeout waiting for HTTP response on ${data.action}`));
-        }, 30000);
-
-        if (data.request_id) {
-          this.pendingResponses.set(data.request_id, (msg) => {
-            clearTimeout(timer);
-            resolve(msg);
-          });
-        }
-
-        try {
-          await handler(data);
-        } catch (err) {
-          clearTimeout(timer);
-          if (data.request_id) {
-            this.pendingResponses.delete(data.request_id);
-          }
-          reject(err);
-        }
-      });
+      responseData = await handler(data);
     } catch (error) {
       if (error instanceof ZodError) {
         status = 400;
@@ -209,15 +173,14 @@ class HTTPTransportAdapter implements TransportAdapter {
     } finally {
       res.writeHead(200, { 'Content-Type': 'application/json' });
 
-      if (!response) {
-        response = {
-          action: data.action,
-          correlation_id: data.correlation_id,
-          data: {},
-          error: errorMessage,
-          status
-        };
-      }
+      const response: CorrelatedResponseDTO = {
+        action: data.action,
+        correlation_id: data.correlation_id,
+        data: responseData ?? {},
+        error: errorMessage,
+        status
+      };
+
       res.end(JSON.stringify(response));
     }
   }
